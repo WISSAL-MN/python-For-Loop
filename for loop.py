901e3bfb11f0f0b6a32fda430903cace7698f0b9
Wissal_m.n <wissalmanseri@gmail.com>
2022-03-12 22:41:10 +0100

    Update for loop.py

--- for loop.py
+++ for loop.py
@@ -14,3 +14,14 @@ for x in fruits:
   print(x) 
   for x in range(6):
   print(x)
+  for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
